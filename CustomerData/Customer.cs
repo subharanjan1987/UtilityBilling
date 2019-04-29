3bb996159360dc630c4015b72e9573f54b71d1a3
773649 <773649@ICTVM-375B3VVV7>
2019-04-29 08:10:57 -0600

    'update'

---
+++ CustomerData/Customer.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CustomerData
+{
+    public abstract class Customer 
+    {
+        //Define Properties which are auto-implemented
+        //gets and sets corresponding private instance variables  under the hood
+        public string CustomerName
+        { get; set; }    
+        public int CustomerAC
+        { get; set; }
+        public char CustomerType
+        { get; set; }
+        public double CustomerCharge //this property value is set by calculated value from the calculateBill method
+        { get; set; } 
+
+        //defining Constructor
+        public Customer (string s, int a, char t)  //passing parametes to get and set customername,customerA/C and customer type
+        {
+            CustomerName = s;  
+            CustomerAC = a;
+            CustomerType = t;
+         }
+
+        //Declaring Methods -
+        //Abstract method cant'be defined but must be overridden by child classes with same method signatures
+        public abstract double calculateBill(int usage, int x=0);
+
+
+        public override string ToString() // overriding toString() method to read record line from file
+        {
+            return CustomerName + "," + CustomerAC.ToString() + "," + CustomerType.ToString() + "," + CustomerCharge.ToString("c");
+        }
+        public string ToText() // to write a record line in a text file
+        {
+            return CustomerName + "," + CustomerAC.ToString() + "," + CustomerType.ToString() + "," + CustomerCharge.ToString();
+        }
+    }
+}
---
+++ CustomerData/Industrial.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CustomerData
+{
+    public class Industrial : Customer
+    {
+        const int ONPEAKRATE = 76; //Declaring Constant variable to assign value
+        const int OFFPEAKRATE = 40; //Declaring Constant variable to assign value
+
+
+        //Define Constructors
+        public Industrial(string s, int a, char t) : base(s, a, t) { }
+
+        //This method calculates the bill for Industrial type of Cusotmers based on Business Rule
+        public override double calculateBill(int usage, int offpeak)
+        {
+            //calculate energy usage bill fo industrial customer
+
+            double onPeakBill;
+            double offPeakBill;
+            double customerCharge;
+
+            if (usage >= 0 && usage <= 1000)
+            {
+                onPeakBill = ONPEAKRATE;
+            }
+            else { onPeakBill = (double)(ONPEAKRATE + (usage - 1000) * 0.065); }
+            if (offpeak >= 0 && offpeak <= 1000)
+            {
+                offPeakBill = OFFPEAKRATE;
+            }
+            else { offPeakBill = (double)(OFFPEAKRATE + (offpeak - 1000) * 0.028); }
+
+            customerCharge = onPeakBill + offPeakBill;
+            return customerCharge;
+        }
+    }
+}
